a5ae7d7504ebc9cb04d15f95c7abf7e1ab5f056e
Dmitry Balakov <dmitry@balakov.net>
2014-02-03 21:39:39 +0400

    For&while

--- public/js/for.js
+++ public/js/for.js
@@ -19,3 +19,15 @@ while(i < 10) {
     }
     i++;
 }
+
+var myMath = {
+    f : function(n) {
+        var result = 1;
+        for (var i = 2; i < n + 1; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+};
+
+console.log(myMath.f(10));
